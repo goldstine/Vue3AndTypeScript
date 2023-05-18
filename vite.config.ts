b5c485cdf8e73ab3666518500c08a77660bccf33
goldstine <15797899690@163.com>
2023-05-18 17:44:34 +0800

    提交格式化代码

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server: { // ← ← ← ← ← ←
-    host: '0.0.0.0' // ← 新增内容 ←
-  } // ← ← ← ← ← ←
+  server: {
+    // ← ← ← ← ← ←
+    host: '0.0.0.0', // ← 新增内容 ←
+  }, // ← ← ← ← ← ←
 })
