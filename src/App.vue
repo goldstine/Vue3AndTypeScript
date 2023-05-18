9e9732bb4b5d893485078027e2b1b7fefc864965
goldstine <15797899690@163.com>
2023-05-18 19:23:55 +0800

    图标组件

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <el-button type="primary" size="default" @click="">主要按钮</el-button>
+    <el-button type="primary" size="default" :icon="Plus">主要按钮</el-button>
+    <el-button type="success" size="small" :icon="Edit">编辑按钮</el-button>
   </div>
 </template>
 
